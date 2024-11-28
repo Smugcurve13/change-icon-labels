0e22aa3d92f977b6c8b6092a50b000035ad18188
Smugcurve13 <sambhavsoni14@gmail.com>
2024-11-29 02:41:29 +0530

    second script

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
 
-# Mapping of new names for your desktop icons
-icon_names = {
-    "division2": "-",
+
+# Mapping of current names (without ".lnk") to new desired names
+icon_renames = {
+    "division2": "2",
     "fortnite": "fortnite",
     "Grand Theft Auto V": "Grand Theft Auto V",
     "DRAGON BALL": "DRAGON BALL",
@@ -17,26 +18,31 @@ icon_names = {
     "yakuza gaiden": "yakuza gaiden",
     "Palworld": "Palworld",
     "Wuthering Waves": "Wuthering Waves",
-    "Brotato": "Brotato"
+    "Brota": "Brota"
 }
 
-# Path to the desktop
+# Get the path to the desktop
 desktop_path = os.path.join(os.environ["USERPROFILE"], "Desktop")
 
-# Rename the icons on the desktop
+
+
+# Process each file on the desktop
 for file_name in os.listdir(desktop_path):
     # Check if the file is a shortcut (.lnk file)
     if file_name.endswith(".lnk"):
-        # Remove the ".lnk" extension to get the base name
+        # Extract the base name without the ".lnk" extension
         base_name = file_name[:-4]
-        # Check if this icon matches one of the keys in our dictionary
-        if base_name in icon_names:
-            # Construct the new file name
-            new_name = icon_names[base_name] + ".lnk"
+        # Check if the base name exists in the mapping
+        if base_name in icon_renames:
+            # Get the new name from the mapping
+            new_name = icon_renames[base_name] + ".lnk"
             # Rename the file
             old_path = os.path.join(desktop_path, file_name)
             new_path = os.path.join(desktop_path, new_name)
-            os.rename(old_path, new_path)
-            print(f'Renamed "{file_name}" to "{new_name}"')
+            try:
+                os.rename(old_path, new_path)
+                print(f'Renamed "{file_name}" to "{new_name}"')
+            except Exception as e:
+                print(f'Error renaming "{file_name}": {e}')
 
-print("Renaming completed.")
+print("Renaming process completed.")
